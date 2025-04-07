da212376f8340f4bb04888a7667bdc31d8a97d05
sangyeobhan <sangyeobhandev@gmail.com>
2025-04-08 08:34:40 +0900

    557. Reverse Words in a String III

---
+++ Leetcode/SlidingWindows/557_ReverseWordsInString/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        int l = 0;
+        
+        int r;
+        for (r = 0; r < s.length(); r++) {
+            if (s.charAt(r) != ' ') {
+                continue;
+            }
+            for (int i = r - 1; i >= l; i--) {
+                sb.append(s.charAt(i));
+            }
+            l = r + 1;
+            sb.append(' ');
+        }
+
+        for (int i = r - 1; i >= l; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
